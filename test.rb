c59b6e71101a19149c38f1fe16754cb4f634c235
Eric Larson <ewlarson@gmail.com>
2013-03-14 08:44:39 -0500

    Add MongoDB as storage layer

--- test.rb
+++ test.rb
@@ -1,22 +1,28 @@
 require 'anemone'               # Ruby web spider
-require 'capybara/poltergeist'  # Headless browsing and screenshot capture
+require 'mongo'                 # MongoDB
+require 'capybara/webkit'       # Open URL
+require 'capybara-screenshot'   # Save image
 require 'cgi'                   # URL escaping
-require 'csv'                   # Write CSV file
 require 'png_quantizator'       # Convert 32 to 8 PNGs - brew install pngquant
 
-Capybara.default_driver = :poltergeist
+Capybara.default_driver = :webkit
 
 module Hoarder
   extend Capybara::DSL
   
+  # MongoDB
+  @db = Mongo::Connection.new.db("hoarder")
+  @pages = @db["pages"]
+  
+  # Walk the domain
   def self.crawl(url, page_count=100)
-    Anemone.crawl(url, {:delay => 1, :obey_robots_txt => true}) do |anemone|
-      @titles = []
+    Anemone.crawl(url, {:delay => 2, :obey_robots_txt => true}) do |anemone|
+      anemone.storage = Anemone::Storage.MongoDB
       anemone.on_every_page do |page|
         begin
           process_page(page)
         rescue Exception => e
-          puts "Exception - #{e}: #{page.inspect}"
+          puts "Exception - #{e}"
           next
         end
         exit_crawl?(page_count)
@@ -24,26 +30,39 @@ module Hoarder
     end
   end
   
+  # Data and image capture
   def self.process_page(page)
-    title = page.doc.at('title') ? page.doc.at('title').inner_html : ""
-    @titles.push title    
-    write_data_to_csv(title,page)
-    save_image(page.url)
+    # Skip redirects and pages with iframes
+    unless page.code == 302 or page.doc.at('iframe')
+      title = page.doc.at('title') ? page.doc.at('title').inner_html : ""
+      
+      if title
+        page = {title: title, url: page.url.to_s, code: page.code, response_time: page.response_time}
+        puts "Inserting #{page.inspect}"
+        @pages.insert page
+      end
+      
+      #write_data_to_csv(title,page)
+      visit(page[:url])
+      save_image(CGI.escape(page[:url]))
+    end
   end
   
-  # Write info to CSV
-  def self.write_data_to_csv(title,page)
-    CSV.open('pages.csv', 'a+') {|csv| csv << [title, page.url, page.code, page.response_time, page.depth]}
-  end
-  
+  # Write page data to CSV
+  #def self.write_data_to_csv(title,page)
+  #  CSV.open('pages.csv', 'a+') {|csv| csv << [title, page.url, page.code, page.response_time, page.depth]}
+  #end
+
+  # Save optimized screenshot PNG
   def self.save_image(url)
-    visit(url)
-    save_screenshot("#{CGI.escape(url.to_s)}.png", :full => true)
-    PngQuantizator::Image.new("#{CGI.escape(url.to_s)}.png").quantize!
+    saver = Capybara::Screenshot::Saver.new(Capybara, Capybara.page, false, url)
+    saver.save
+    PngQuantizator::Image.new(saver.screenshot_path).quantize!
   end
   
+  # Shall we keep crawling?
   def self.exit_crawl?(page_count)
-    exit if @titles.size == page_count
+    exit if @pages.count == page_count
   end
 end
 
