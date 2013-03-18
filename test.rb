82a22bc5af0b9861a752e0a244dd51e892bf0d25
Eric Larson <ewlarson@gmail.com>
2013-03-18 13:22:17 -0500

    Add list of file extensions to skip during crawl

--- test.rb
+++ test.rb
@@ -3,20 +3,25 @@ require 'mongo'                 # MongoDB
 require 'capybara/webkit'       # Open URL
 require 'capybara-screenshot'   # Save image
 require 'cgi'                   # URL escaping
+require 'csv'                   # Write CSV file
 require 'png_quantizator'       # Convert 32 to 8 PNGs - brew install pngquant
+
+# @TODO: mongodb + delayed_job integration
+# - Image capture causes a large crawls page_queue to slow until it ultimately hangs
+# - Need to push image capture to background process
 
 Capybara.default_driver = :webkit
 
 module Hoarder
   extend Capybara::DSL
   
-  # MongoDB
-  @db = Mongo::Connection.new.db("hoarder")
-  @pages = @db["pages"]
-  
   # Walk the domain
   def self.crawl(url, page_count=100)
-    Anemone.crawl(url, {:delay => 2, :obey_robots_txt => true}) do |anemone|
+    Anemone.crawl(url, {:delay => 2, :obey_robots_txt => true, :skip_query_strings => true}) do |anemone|
+      # Do not crawl doc extensions
+      # @TODO: Don't get trapped on calendars
+      ext = %w(flv swf png jpg gif asx zip rar tar 7z gz jar js css dtd xsd ico raw mp3 mp4 wav wmv ape aac ac3 wma aiff mpg mpeg avi mov ogg mkv mka asx asf mp2 m1v m3u f4v pdf doc xls ppt pps bin exe rss xml)
+      anemone.skip_links_like(/\.#{ext.join('|')}$/)
       anemone.storage = Anemone::Storage.MongoDB
       anemone.on_every_page do |page|
         begin
@@ -25,7 +30,7 @@ module Hoarder
           puts "Exception - #{e}"
           next
         end
-        exit_crawl?(page_count)
+        #exit_crawl?(page_count)
       end
     end
   end
@@ -34,24 +39,12 @@ module Hoarder
   def self.process_page(page)
     # Skip redirects and pages with iframes
     unless page.code == 302 or page.doc.at('iframe')
-      title = page.doc.at('title') ? page.doc.at('title').inner_html : ""
-      
-      if title
-        page = {title: title, url: page.url.to_s, code: page.code, response_time: page.response_time}
-        puts "Inserting #{page.inspect}"
-        @pages.insert page
-      end
-      
-      #write_data_to_csv(title,page)
-      visit(page[:url])
-      save_image(CGI.escape(page[:url]))
+      page.data = {}
+      page.data[:title] = begin page.doc.at('title').inner_html rescue "" end
+      #visit(page.url.to_s)
+      #save_image(CGI.escape(page.url.to_s))
     end
   end
-  
-  # Write page data to CSV
-  #def self.write_data_to_csv(title,page)
-  #  CSV.open('pages.csv', 'a+') {|csv| csv << [title, page.url, page.code, page.response_time, page.depth]}
-  #end
 
   # Save optimized screenshot PNG
   def self.save_image(url)
@@ -62,8 +55,8 @@ module Hoarder
   
   # Shall we keep crawling?
   def self.exit_crawl?(page_count)
-    exit if @pages.count == page_count
+    #exit if @pages.count == page_count
   end
 end
 
-Hoarder.crawl('http://library.wisc.edu', 10)
+Hoarder.crawl('http://library.wisc.edu', 100)
